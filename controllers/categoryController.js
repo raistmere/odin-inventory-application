3684cf9ff73381fb7c29e51f9efa3dbb23910d6c
raistmere <raistmere@gmail.com>
2024-04-19 15:48:33 -0500

    feat: added item list to category_detail view
    category_detail view will now show a minimal list of items
    that are under that category. Each item is linked to redirect the
    user to the selected item detail page. I also fixed some minor
    styling issues with the category description.

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,9 +1,9 @@
 const Category = require("../models/category.js");
+const Item = require("../models/item.js");
 
 // Display the inventory category list
 exports.get_category_list = async (req, res, next) => {
     const allCategories = await Category.find({});
-    console.log(allCategories);
 
     res.render('category_list', { 
         title: "Category List",
@@ -14,6 +14,12 @@ exports.get_category_list = async (req, res, next) => {
 // Displays specific category detail based on id
 exports.get_category_detail = async(req, res, next) => {
     const category = await Category.findById(req.params.id);
+    const categoryItemList = await Item.find({category: category.id});
+    console.log(categoryItemList);
 
-    res.render("category_detail", { categoryDetail: category});
+    res.render("category_detail", 
+    { 
+        categoryDetail: category,
+        categoryItemList: categoryItemList
+    });
 }
